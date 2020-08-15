6b44ce9059d4d382649498137ae9c900939409fa
Harrison Grieve <rhgrieve@hey.com>
2020-08-15 02:59:02 -0400

    Comment key render log / trigger vercel build

--- src/components/TreeKey.js
+++ src/components/TreeKey.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, css } from "aphrodite";
 
-export const TreeKey = ({ query, text }) => {
+const TreeKey = ({ query, text }) => {
   const [prefix, suffix] = text.split(query);
+
+  useEffect(() => {
+    // console.log("rendering key");
+  }, []);
 
   return (
     <span className={css(styles.spacing)}>
@@ -30,3 +34,5 @@ const styles = StyleSheet.create({
     padding: "0.2em 0"
   }
 });
+
+export default React.memo(TreeKey);
