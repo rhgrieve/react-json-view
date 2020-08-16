f2fa3d2d8dacbc134ee04d5433dfdebbe7ec84b5
Harrison Grieve <rhgrieve@hey.com>
2020-08-16 04:09:31 -0400

    Remove uuid from HighlightQuery.js

--- src/components/HighlightQuery.js
+++ src/components/HighlightQuery.js
@@ -1,20 +1,19 @@
 import React from "react";
-import uuid from "react-uuid";
 import { StyleSheet, css } from "aphrodite";
 
 const HighlightQuery = ({ query, children }) => {
   const splitRegex = new RegExp(`(?<=${query})|(?=${query})`, "i");
   const splitText = children.split(splitRegex);
-  console.log(splitText);
+  // console.log(splitText);
 
   // let [prefix, suffix] = children.split(query, 2);
 
   return (
     <>
-      {splitText.map((chars) => {
+      {splitText.map((chars, i) => {
         const charsAreQuery = chars.toLowerCase() === query.toLowerCase();
         return (
-          <span key={uuid()} className={charsAreQuery ? css(styles.bold) : ""}>
+          <span key={i} className={charsAreQuery ? css(styles.bold) : ""}>
             {chars}
           </span>
         );
@@ -31,4 +30,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default HighlightQuery;
+export default React.memo(HighlightQuery);
